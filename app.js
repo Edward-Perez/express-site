316a1b2d71fdbde494ccab43796bf84b08748e7f
Edward Perez <edward.121489@gmail.com>
2019-05-18 14:52:45 +0200

    Add/remove comments

--- app.js
+++ app.js
@@ -3,23 +3,28 @@ const app = express();
 const port = 3000;
 const index = require('./routes/index.js');
 
+// Set view engine
 app.set('view engine', 'pug');
+// Static method to serve static files
 app.use('/static', express.static('public'));
+// Use routes in index
 app.use(index);
 
+// Create error constructor
 app.use( (req, res, next) => {
     const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
   
+// Display error 
 app.use( (err, req, res, next) => {
     res.locals.error = err;
     console.error(`There is a ${err.status} error.`);
     res.render('error');
 });
 
-
+// Create server
 app.listen(port, () => {
     console.log(`listening on ${port}`);
 })
--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,18 @@ const express = require('express');
 const router = express.Router();
 const { projects } = require('../data.json');
 
-
+// Routes to "index" page
 router.get('/', (req, res, next) => {
     res.locals.projects = projects;
     res.render('index');
 });
 
+// Routes to "about" page
 router.get('/about', (req, res, next) => {
     res.render('about');
 });
 
+// Routes to a "project" page, if not project "id" variable is present redirect to index page 
 router.get('/project/:id', (req, res, next) => {
     const id = req.params;
     res.locals.projects = projects;
